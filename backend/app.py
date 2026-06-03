8e9b4a46b247fe13dc4ab5616aa1974e14a31bb1
Tanishq Gupta <tanishqgupta949@gmail.com>
2026-06-03 19:37:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Backend API Running Successfully"
+
+@app.route('/api')
+def api():
+    return "API Endpoint Working"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
